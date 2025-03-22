2d2be8e2eadcaff6a7a84cd0387b39e06b245613
Julionate <julionate@proton.me>
2025-03-22 18:08:11 -0300

    Added and configured DaisyUI.

---
+++ src/components/Buttons/ThemeToggleBtn.tsx
@@ -0,0 +1,9 @@
+import { useSignal } from "@preact/signals";
+
+export const ThemeToggleBtn = () => {
+  return (
+    <div className="dropdown">
+      <div class="btn btn-primary">Hello</div>
+    </div>
+  );
+};
