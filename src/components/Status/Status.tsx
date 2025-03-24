b577769d6946f3af38109bcd621bd42a338663b9
Julionate <julionate@proton.me>
2025-03-24 10:22:27 -0300

    Created Status based on my schedule

---
+++ src/components/Status/Status.tsx
@@ -0,0 +1,46 @@
+import { useSignal } from "@preact/signals";
+import { useTranslations } from "../../i18n/utils";
+import { languages } from "../../i18n/ui";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+export const Status = ({ lang }: { lang: keyof typeof languages }) => {
+  const available = useSignal<boolean>(false);
+  const t = useTranslations(lang);
+
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+
+  const fromHour = dayjs.tz("2025-03-23 11:00", "America/Santiago");
+  const toHour = dayjs.tz("2025-03-23 17:00", "America/Santiago");
+
+  const fromLocalHour = fromHour.local().hour();
+  const toLocalHour = toHour.local().hour();
+  const currentLocalHour = dayjs.tz(undefined, dayjs.tz.guess()).hour();
+
+  available.value =
+    currentLocalHour >= fromLocalHour && currentLocalHour <= toLocalHour
+      ? true
+      : false;
+
+  if (!available.value) {
+    return (
+      <div class="w-max h-max bg-red-500/10 rounded-full p-2 flex items-center gap-2">
+        <div class="w-3 h-3 rounded-full bg-red-400" />
+        <span class="text-red-900 dark:text-red-400 font-medium">
+          {t("available.off")}
+        </span>
+      </div>
+    );
+  }
+
+  return (
+    <div class="w-max h-max bg-green-500/10 rounded-full p-2 flex items-center gap-2">
+      <div class="w-3 h-3 rounded-full bg-green-400" />
+      <span class="text-green-900 dark:text-green-400 font-medium">
+        {t("available.on")}
+      </span>
+    </div>
+  );
+};
